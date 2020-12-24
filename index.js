ed660eb85e473c9d7d23d9b0272579222c37659d
Dhruv2110 <dhruvsharma.ynr@gmail.com>
2020-12-24 20:48:42 +0530

    Latest

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const popup = require('node-popup');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/public' , express.static('public'));
+
+app.get('/',(req,res,next) => {
+    res.sendFile('D:/Practice/Node/Login/index.html');
+    //res.send('HEllo');
+
+})
+
+app.get('/login',(req,res,next) => {
+    res.redirect('/');
+})
+
+app.post('/login',(req,res,next) => {
+    const username = req.body.email;
+    const password = req.body.pass;
+    //console.log(username,password);
+
+    if(username == 'abc' && password == '123')
+    {
+        res.sendFile('D:/Practice/Node/Login/success.html');
+    }
+    else{ 
+        res.sendFile('D:/Practice/Node/Login/error.html');
+    }
+
+
+})
+
+
+app.listen(8000,() => {
+    console.log('Listening on 8000',__dirname);
+
+})
